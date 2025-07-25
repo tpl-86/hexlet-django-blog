a28f80136ab9fe8ad693e72574aaf2c3298b295e
tpl-86 <tron-050686@mail.ru>
2025-07-25 14:30:24 +0300

    Created a model Article

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
