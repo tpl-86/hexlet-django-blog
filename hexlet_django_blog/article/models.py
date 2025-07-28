dff1e1287021c9dd9a18a25fdc70d6f6f7650374
tpl-86 <tron-050686@mail.ru>
2025-07-28 12:12:09 +0300

    Added Article to Admin

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -6,4 +6,7 @@ class Article(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
     
